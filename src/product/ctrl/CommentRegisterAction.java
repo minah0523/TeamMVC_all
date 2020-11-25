6b6917ed4fba2b2b2a03438d04566f0479b60fee
Mina Kim <64657792+minah0523@users.noreply.github.com>
2020-11-25 12:49:02 +0900

    1125 update2

--- src/product/ctrl/CommentRegisterAction.java
+++ src/product/ctrl/CommentRegisterAction.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package product.ctrl;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,40 +33,3 @@ public class CommentRegisterAction extends AbstractController {
 	}
 
 }
-=======
-package product.ctrl;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import common.ctrl.AbstractController;
-import my.util.MyUtil;
-import myshop.mdl.*;
-
-public class CommentRegisterAction extends AbstractController {
-
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		String fk_userid = request.getParameter("fk_userid");
-		String fk_pdno = request.getParameter("fk_pdno");
-		String contents = request.getParameter("contents");
-		
-		// **** 크로스 사이트 스크립트 공격에 대응하는 안전한 코드(시큐어코드/secure code) 작성하기 **** //
-		contents = MyUtil.secureCode(contents);
-		
-		contents = contents.replaceAll("\r\n", "<br>");
-		
-		PurchaseReviewsVO reviewsvo = new PurchaseReviewsVO();
-		reviewsvo.setFk_userid(fk_userid);
-		reviewsvo.setFk_pdno(Integer.parseInt(fk_pdno) );
-		reviewsvo.setContents(contents);
-		
-		InterProductDAO pdao = new ProductDAO();
-		pdao.addComment(reviewsvo);
-		
-
-	}
-
-}
->>>>>>> 642543c879f749d082a1e969d8b35798f725c4b9
