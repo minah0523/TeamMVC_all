81941a25a5812eadde86e0a13df4dc003cc24179
ashgray611 <74528041+ashgray611@users.noreply.github.com>
2020-11-30 09:30:21 +0900

    Update LikeAction.java

--- src/product/ctrl/LikeAction.java
+++ src/product/ctrl/LikeAction.java
@@ -15,17 +15,29 @@ public class LikeAction extends AbstractController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		String pdno = request.getParameter("pdno");
-		String userid = request.getParameter("userid");
+		InterProductDAO pdao = new ProductDAO();
+		
+		
+		String pdno = "";
+		pdno = request.getParameter("pdno");
+		String userid = "";
+		userid = request.getParameter("userid");
+		
+		System.out.println("likeact pdno1 : " + pdno);
+		System.out.println("likeact userid1 : " + userid);
 		
 		Map<String,String> paraMap = new HashMap<>();
 		paraMap.put("pdno", pdno);
 		paraMap.put("userid", userid);
 		
-		InterProductDAO pdao = new ProductDAO();
-		
+		System.out.println("likeact pdno2 : " + pdno);
+		System.out.println("likeact userid2 : " + userid);
 		int n = pdao.like(paraMap);
 		// n => 1 이라면 정상투표,  n => 0 이라면 중복투표
+		
+		System.out.println("likeact pdno3 : " + pdno);
+		System.out.println("likeact userid3 : " + userid);
+		System.out.println("likeact n = " + n);
 		
 		String msg = "";
 		
@@ -38,12 +50,14 @@ public class LikeAction extends AbstractController {
 		
 		JSONObject jsonObj = new JSONObject();
 		jsonObj.put("msg", msg); // {"msg":"해당제품에\n 좋아요를 클릭하셨습니다."}   {"msg":"이미 좋아요를 클릭하셨기에\n 두번 이상 좋아요는 불가합니다."} 
+		System.out.println("likeact jsonObj : " + jsonObj);
 		
 		String json = jsonObj.toString(); // "{"msg":"해당제품에\n 좋아요를 클릭하셨습니다."}"   "{{"msg":"이미 좋아요를 클릭하셨기에\n 두번 이상 좋아요는 불가합니다."}}" 
+		System.out.println("likeact json : " + json);
 		
 		request.setAttribute("json", json);
-		
-	//	super.setRedirect(false);
+		System.out.println("likeact json2 : " + json);
+		//	super.setRedirect(false);
 		super.setViewPage("/WEB-INF/jsonview.jsp");
 	}
 
