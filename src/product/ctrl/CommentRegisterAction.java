a4c45ad0e72b03cc50d8c3173e0902ce1f4f3a49
ashgray611 <74528041+ashgray611@users.noreply.github.com>
2020-12-03 21:05:53 +0900

    Update CommentRegisterAction.java
    201203 2100

--- src/product/ctrl/CommentRegisterAction.java
+++ src/product/ctrl/CommentRegisterAction.java
@@ -12,9 +12,12 @@ public class CommentRegisterAction extends AbstractController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		String fk_userid = request.getParameter("fk_userid");
-		String fk_pdno = request.getParameter("fk_pdno");
-		String contents = request.getParameter("contents");
+		String userid = "";
+		userid = request.getParameter("fk_userid");
+		String pdno = "";
+		pdno = request.getParameter("fk_pdno");
+		String contents = "";
+		contents = request.getParameter("contents");
 		
 		// **** 크로스 사이트 스크립트 공격에 대응하는 안전한 코드(시큐어코드/secure code) 작성하기 **** //
 		contents = MyUtil.secureCode(contents);
@@ -22,8 +25,8 @@ public class CommentRegisterAction extends AbstractController {
 		contents = contents.replaceAll("\r\n", "<br>");
 		
 		PurchaseReviewsVO reviewsvo = new PurchaseReviewsVO();
-		reviewsvo.setFk_userid(fk_userid);
-		reviewsvo.setFk_pdno(Integer.parseInt(fk_pdno) );
+		reviewsvo.setFk_userid(userid);
+		reviewsvo.setFk_pdno(Integer.parseInt(pdno) );
 		reviewsvo.setContents(contents);
 		
 		InterProductDAO pdao = new ProductDAO();
