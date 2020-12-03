1603f54e060cbdffc5a75d0306b5069988975f20
ashgray611 <74528041+ashgray611@users.noreply.github.com>
2020-12-03 21:02:27 +0900

    Update GoaddcartAction.java
    201203_2100

--- src/product/ctrl/GoaddcartAction.java
+++ src/product/ctrl/GoaddcartAction.java
@@ -33,18 +33,11 @@ public class GoaddcartAction extends AbstractController {
 		String color = request.getParameter("color");
 		String size = request.getParameter("size");
 		String pdno = request.getParameter("pdno");
-		
-		System.out.println("GoaddcartAct- color ==> " + color);
-		System.out.println("GoaddcartAct- size ==> " + size);
-		System.out.println("GoaddcartAct- pdno ==> " + pdno);
-		
-		
+				
 		// 이건 별도로 카트에 담아줄것
 		String pqty = request.getParameter("pqty");
-		System.out.println("GoaddcartAct- pqty ==>" + pqty);
 		
 		String pinfono = pdao.getPinfono(color, size, pdno);
-		System.out.println("GoaddcartAct- pinfono ==>" + pinfono);
 		
 		Map<String,String> cartMap = new HashMap<>();
 		
@@ -54,29 +47,9 @@ public class GoaddcartAction extends AbstractController {
 		cartMap.put("pqty", pqty);
 		cartMap.put("pinfono", pinfono);
 		
-		System.out.println("GoaddcartAct Map Check : " + cartMap);
-		
-		
 		int CartList = pdao.sendCartList(cartMap);
 		
 		request.setAttribute("CartList", CartList);
-		
-		
-		/*pinfono = request.getParameter("pinfono");
-		 *request.setAttribute("pinfono", pinfono);
-		 *System.out.println("pinfono G.A.C.A 2nd check : " + pinfono);
-		 *
-		 * String message = ""; // alert 내용 String loc = ""; // 경로
-		 * 
-		 * if(cartMap == 1) { message = "장바구니 등록 완료";
-		 * 
-		 * // message 띄운후 어느 페이지로 갈래? loc = request.getContextPath() +
-		 * "/Payment/Payment."; // 성공하면 리스트 페이지로 가자 }else { message = "장바구니 등록 실패"; loc
-		 * = request.getContextPath() + "/productdetail/ProductDetail.neige"; }
-		 * 
-		 * } }
-		 */
-		
 		
 		//setRedirect(false);
 		super.setViewPage("/WEB-INF/productdetail/ProductDetail.jsp");
