5f1de4695dc5a687e841b0808a44bb8d646bee60
ashgray611 <74528041+ashgray611@users.noreply.github.com>
2020-12-06 19:29:58 +0900

    Update CommentRegisterAction.java

--- src/product/ctrl/CommentRegisterAction.java
+++ src/product/ctrl/CommentRegisterAction.java
@@ -1,5 +1,8 @@
 package product.ctrl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,11 +16,15 @@ public class CommentRegisterAction extends AbstractController {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		String userid = "";
-		userid = request.getParameter("fk_userid");
+		userid = request.getParameter("userid");
 		String pdno = "";
-		pdno = request.getParameter("fk_pdno");
+		pdno = request.getParameter("pdno");
 		String contents = "";
 		contents = request.getParameter("contents");
+		
+		//System.out.println("commentRegister check userid1" + userid);
+		//System.out.println("commentRegister check pdno2" + pdno);
+		//System.out.println("commentRegister check content1" + contents);
 		
 		// **** 크로스 사이트 스크립트 공격에 대응하는 안전한 코드(시큐어코드/secure code) 작성하기 **** //
 		contents = MyUtil.secureCode(contents);
@@ -28,6 +35,9 @@ public class CommentRegisterAction extends AbstractController {
 		reviewsvo.setFk_userid(userid);
 		reviewsvo.setFk_pdno(Integer.parseInt(pdno) );
 		reviewsvo.setContents(contents);
+		
+		//System.out.println("commentRegister check content2" + contents);
+		
 		
 		InterProductDAO pdao = new ProductDAO();
 		pdao.addComment(reviewsvo);
